d368919df4517de558a71b45e6469657fd4c6f6f
bella <theboss@ciaobella.me>
2021-06-10 10:44:57 -0700

    first

---
+++ ahck.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace ahck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
+
+
+var friendName = "kyle";
+int year = 2021;
+char z = 'x';
+bool iscool = true;
+double cup = 0.5;
+decimal money = 420.50m;
+
+Console.WriteLine($"My friend's name is {friendName} he was born in {year}. He has {money} in his bank account.I asked him if he was cool and he said {iscool}. His favorite letter is {z} which is also z. He has a water bottle that is {cup} full.");
